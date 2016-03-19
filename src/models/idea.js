5ff24c3cf5c45b694120061771ddae6693c37974
timwis <tim@timwis.com>
2016-03-19 18:52:58 -0400

    add voting

--- src/models/idea.js
+++ src/models/idea.js
@@ -1,11 +1,24 @@
 import Model from 'ampersand-model'
+import Firebase from 'firebase'
+import {result} from 'lodash'
 
 export default Model.extend({
   props: {
     title: 'string',
-    description: 'string'
+    description: 'string',
+    votes: ['number', false, 0]
   },
   session: {
     id: 'string'
+  },
+  initialize: function () {
+    this.firebase = new Firebase(result(this, 'url'))
+  },
+  url: function () {
+    const boardId = this.collection.parent.id
+    return `https://influent.firebaseio.com/boards/${boardId}/ideas/${this.id}`
+  },
+  castVote: function () {
+    this.firebase.update({votes: this.votes + 1})
   }
 })
--- src/views/idea-item.js
+++ src/views/idea-item.js
@@ -6,6 +6,15 @@ export default View.extend({
   template: Template,
   bindings: {
     'model.title': '[data-hook~=idea-title]',
-    'model.description': '[data-hook~=idea-description]'
+    'model.description': '[data-hook~=idea-description]',
+    'model.votes': '[data-hook~=idea-votes]'
+  },
+  events: {
+    'click [data-hook~=cast-vote]': 'onCastVote'
+  },
+  onCastVote: function (e) {
+    // this.trigger('castVote')
+    this.model.castVote()
+    e.preventDefault()
   }
 })
